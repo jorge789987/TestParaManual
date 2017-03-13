f045c54d004eef5cdfa523e428cd76b64627ebbf
Jorge usuario <Jorge usuario@192.168.1.34>
2017-03-13 14:36:09 +0100

    Segundo commit

--- JorgeOjalvo/src/demos/programita.java
+++ JorgeOjalvo/src/demos/programita.java
@@ -4,8 +4,13 @@ public class programita {
 	
 	public void metodo1(){}
 	
-	public void metodo2(){}
+	public void metodo2(){
+		
+		System.out.println("Test para otro commit");
+	}
 	
 	public void metodo3(){}
+	
+	public void metodo4(){}
 
 }
